3d8fb9f188fd061398ad59edfe43f9a590e4fd14
Shane Becker <veganstraightedge@gmail.com>
2025-10-15 03:28:57 -0700

    New gem

---
+++ lib/numruby/plot.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "plot/version"
+
+module Numruby
+  module Plot
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/numruby/plot/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Numruby
+  module Plot
+    VERSION = "0.1.0"
+  end
+end
